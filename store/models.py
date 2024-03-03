9b8d9eedb0e20bf4b944fb49570926190f4e8d4a
grecpt <grecpt@gmail.com>
2024-03-03 20:53:17 +1000

    Updat the store app models

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
