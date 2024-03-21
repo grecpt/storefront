5a20366859498be8ac2323b91c4b1d0587385de8
grecpt <grecpt@gmail.com>
2024-03-21 16:54:15 +1000

    Renamed feature_product to featured_product fields

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    feature_product = models.ForeignKey(
+    featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
