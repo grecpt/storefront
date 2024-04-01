56eae412cd531ce644b11073ad2321d9fe86b870
grecpt <grecpt@gmail.com>
2024-04-01 14:06:06 +1000

    Update say_hello function in the views of the playground app.

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 
 
 def say_hello(request):
+    Product.objects
+
     return render(request, 'index.html', {'name': 'Rex'})
